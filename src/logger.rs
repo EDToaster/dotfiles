cef9b0616a49e63026f0b20942db36368d19671f
EDToaster <howard3061@gmail.com>
2025-03-15 11:55:01 -0700

    Cleanup!

--- src/logger.rs
+++ src/logger.rs
@@ -1,56 +1,30 @@
 use colored::{ColoredString, Colorize};
+use log::{Level, Log};
 
-pub struct Logger {
-    level: usize,
-}
+pub struct Logger;
 
-#[derive(Debug, Clone, Copy)]
-pub enum Level {
-    INFO,
-    WARN,
-    ERROR,
-}
-
-impl Level {
-    pub fn prefix(&self) -> ColoredString {
-        match &self {
-            Level::INFO => "[INFO]".green(),
-            Level::WARN => "[WARN]".yellow(),
-            Level::ERROR => "[ERROR]".red(),
-        }
+fn prefix(level: Level) -> ColoredString {
+    match level {
+        Level::Error => "[ERROR]".red(),
+        Level::Warn => "[WARN]".yellow(),
+        Level::Info => "[INFO]".green(),
+        Level::Debug => "[DEBUG]".dimmed(),
+        Level::Trace => "[TRACE]".dimmed(),
     }
 }
 
-impl Logger {
-    pub fn new() -> Self {
-        Self { level: 0 }
+impl Log for Logger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
     }
 
-    pub fn scope(&self) -> Self {
-        Self {
-            level: self.level + 1,
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} {}", prefix(record.level()), record.args())
         }
     }
 
-    fn inner_print(&self, level: Option<Level>, content: &str) {
-        for line in content.lines() {
-            if let Some(level) = level {
-                println!("{}{} {line}", "\t".repeat(self.level), level.prefix())
-            } else {
-                println!("{}{line}", "\t".repeat(self.level))
-            }
-        }
-    }
-
-    pub fn info(&self, content: &str) {
-        self.inner_print(Some(Level::INFO), content);
-    }
-
-    pub fn warn(&self, content: &str) {
-        self.inner_print(Some(Level::WARN), content);
-    }
-
-    pub fn error(&self, content: &str) {
-        self.inner_print(Some(Level::ERROR), content);
+    fn flush(&self) {
+        todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use anyhow::bail;
 use colored::Colorize;
 use logger::Logger;
-use std::{error::Error, fmt, fs, path::PathBuf, process::Command};
+use profile::Config;
+use std::{
+    fs,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
 use structopt::StructOpt;
 
 use crate::{profile::Profile, util::absolutize};
@@ -25,167 +31,148 @@ struct Opt {
     profile: PathBuf,
 }
 
-#[derive(Debug)]
-struct ValidationError;
-impl Error for ValidationError {}
-
-impl fmt::Display for ValidationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Validation Error")
-    }
-}
-
+#[derive(Debug, Clone)]
 struct Link {
     install_path: PathBuf,
     parent_path: PathBuf,
     target_path: PathBuf,
 }
-type Links = Vec<Link>;
 
-/// For each config, we will run the following validations:
-/// * Run validation commands specified in the dotfile
-/// * Check the target path exists, and
-/// * Check the install location is specified in the profile
-/// * Check the install location doesn't already have a file
-///   that is not a symlink
-/// * Check the install parent path exists. If not, emit a warning.
-/// Returns (number of warnings, number of errors)
-fn validate(profile_base_path: &PathBuf, prof: &Profile) -> (usize, usize, Links) {
-    let logger = Logger::new();
-
-    let mut errs = 0usize;
-    let mut opt_errs = 0usize;
-    let mut warns = 0usize;
-    let mut links: Links = vec![];
-    for config in &prof.configs {
-        logger.info(&format!("{} {config}", "Validating".green()));
-
-        let logger = logger.scope();
-
-        // Run pre-validation commands on the profile
-        for cmd in &prof.validation_commands {
-            logger.info(&format!("Spawning validation command {cmd:?}"));
-            if cmd.is_empty() {
-                errs += 1;
-                logger.error("Validation command cannot be empty.");
-                continue;
-            }
-
-            let proc = Command::new(&cmd[0]).args(&cmd[1..]).spawn();
-
-            match proc {
-                Ok(mut proc) => {
-                    if let Ok(result) = proc.wait() {
-                        if !result.success() {
-                            errs += 1;
-                            logger.error("Validation command failed");
-                        }
-                    } else {
-                        errs += 1;
-                        logger.error("Validation command failed");
-                    }
+fn validate_process(cmd: &[String]) -> anyhow::Result<()> {
+    let cmd_str = cmd.join(" ");
+    let proc = Command::new(&cmd[0])
+        .args(&cmd[1..])
+        .stdout(Stdio::piped())
+        .spawn();
+    match proc {
+        Ok(proc) => {
+            if let Ok(output) = proc.wait_with_output() {
+                let stdout = String::from_utf8(output.stdout);
+                match stdout {
+                    Ok(s) => s
+                        .lines()
+                        .for_each(|l| log::info!("{}", format!("    Stdout: {l}").dimmed())),
+                    Err(_) => log::info!("Stdout from validation command was not valid UTF-8"),
                 }
-                Err(e) => {
-                    errs += 1;
-                    logger.error(&format!(
-                        "Something went wrong with spawning the validation command: {e}"
-                    ));
+
+                let status = output.status;
+                if !status.success() {
+                    bail!(
+                        "Validation command failed: {cmd_str:?}. Return code was {:?}",
+                        status.code()
+                    );
                 }
+            } else {
+                bail!("Unable to wait for completion of validation command: {cmd_str:?}");
             }
         }
+        Err(e) => {
+            bail!("Something went wrong with spawning the validation command: {cmd_str:?}. {e}");
+        }
+    }
+    Ok(())
+}
 
-        for cmd in &prof.optional_validation_commands {
-            logger.info(&format!("Spawning optional validation command {cmd:?}"));
+fn validate_config(
+    config: &Config,
+    profile_base_path: &PathBuf,
+    prof: &Profile,
+) -> anyhow::Result<Link> {
+    log::info!("{} {config}", "Validating".green());
 
-            if cmd.is_empty() {
-                errs += 1;
-                logger.error("Validation command cannot be empty.");
-                continue;
-            }
+    let iter = prof
+        .validation_commands
+        .iter()
+        .cloned()
+        .map(|c| (c, false))
+        .chain(
+            prof.optional_validation_commands
+                .iter()
+                .cloned()
+                .map(|c| (c, true))
+                .collect::<Vec<_>>(),
+        );
 
-            let proc = Command::new(&cmd[0]).args(&cmd[1..]).spawn();
-
-            match proc {
-                Ok(mut proc) => {
-                    if let Ok(result) = proc.wait() {
-                        if !result.success() {
-                            opt_errs += 1;
-                            logger.warn("Validation command failed");
-                        }
-                    } else {
-                        opt_errs += 1;
-                        logger.warn("Validation command failed");
-                    }
-                }
-                Err(e) => {
-                    opt_errs += 1;
-                    logger.warn(&format!(
-                        "Something went wrong with spawning the validation command: {e}"
-                    ));
-                }
-            }
+    // Run pre-validation commands on the profile
+    for (cmd, optional) in iter {
+        let cmd_str = cmd.join(" ");
+        log::info!("Spawning validation command (not escaped) {cmd_str:?}",);
+        if cmd.is_empty() {
+            bail!("Validation command cannot be empty.");
         }
 
-        let mut target_path = profile_base_path.clone();
-        target_path.push(&config.path);
-        let target_path = absolutize(&target_path.display().to_string());
+        let res = validate_process(&cmd);
 
-        // check target path exists
-        if !target_path.exists() {
-            errs += 1;
-            logger.error(&format!("Target path {target_path:?} does not exist."));
+        match res {
+            Err(e) if optional => log::warn!("{e}"),
+            Err(e) => bail!(e),
+            Ok(_) => {}
         }
-
-        // check the install location is specified in the profile
-        let install_path = config.get_install_location();
-        if let Some(install_path) = install_path {
-            let parent_path = install_path.parent();
-
-            // check install location doesn't already have a file
-            if install_path.exists() {
-                if install_path.is_symlink() {
-                    warns += 1;
-                    logger.warn("Install path exists as a symlink already.");
-                } else {
-                    errs += 1;
-                    logger.error("Install path already exists as a file. Delete this file and rerun validations if it is safe to do so.");
-                    continue;
-                }
-            }
-
-            // check parent path already exists
-            match parent_path {
-                None => {
-                    errs += 1;
-                    logger.error("Install path does not have a parent directory.");
-                    continue;
-                }
-                Some(parent_path) => {
-                    if !parent_path.exists() {
-                        warns += 1;
-                        logger.warn("Install path's parent does not exist. It will be created.");
-                    }
-                    // passed validation, add to links
-                    links.push(Link {
-                        install_path: install_path.clone(),
-                        parent_path: parent_path.into(),
-                        target_path,
-                    });
-                }
-            }
-        } else {
-            errs += 1;
-            logger.error(&format!(
-                "Install path is not specified for platform {}",
-                std::env::consts::OS
-            ));
-            continue;
-        }
-
-        println!();
     }
 
-    (warns, errs, links)
+    let mut target_path = profile_base_path.clone();
+    target_path.push(&config.path);
+    let target_path = absolutize(&target_path.display().to_string());
+
+    // check target path exists
+    if !target_path.exists() {
+        bail!("Target path {target_path:?} does not exist.");
+    }
+
+    // check the install location is specified in the profile
+    let install_path = config.get_install_location();
+    if let Some(install_path) = install_path {
+        let parent_path = install_path.parent();
+
+        // check install location doesn't already have a file
+        if install_path.exists() {
+            if install_path.is_symlink() {
+                log::warn!("Install path exists as a symlink already.");
+            } else {
+                bail!("Install path already exists as a file. Delete this file and rerun validations if it is safe to do so.");
+            }
+        }
+
+        // check parent path already exists
+        match parent_path {
+            None => {
+                bail!("Install path does not have a parent directory.");
+            }
+            Some(parent_path) => {
+                if !parent_path.exists() {
+                    log::warn!("Install path's parent does not exist. It will be created.");
+                }
+                // passed validation, add to links
+                Ok(Link {
+                    install_path: install_path.clone(),
+                    parent_path: parent_path.into(),
+                    target_path,
+                })
+            }
+        }
+    } else {
+        bail!(
+            "Install path is not specified for platform {}",
+            std::env::consts::OS
+        );
+    }
+}
+
+fn validate(profile_base_path: &PathBuf, prof: &Profile) -> anyhow::Result<Vec<Link>> {
+    let links: Result<Vec<Link>, _> = prof
+        .configs
+        .iter()
+        .map(|c| validate_config(c, profile_base_path, prof))
+        .inspect(|r| {
+            r.as_ref().inspect_err(|e| log::error!("{e}")).ok();
+        })
+        // Collect to vec first, because we don't want to short circuit on the first failure
+        .collect::<Vec<_>>()
+        .into_iter()
+        .collect();
+
+    // Consume the error since we logged in the previous line already. Just return a generic error.
+    links.map_err(|_| anyhow::anyhow!("There was an error validating the {:?} profile", prof.name))
 }
 
 /// Create a symlink from inst to target.
@@ -193,15 +180,15 @@ fn validate(profile_base_path: &PathBuf, prof: &Profile) -> (usize, usize, Links
 /// Since this is deprecated, change to platform-dependent implmentation
 /// when allowing for whole-directory symlinks.
 #[allow(deprecated)]
-fn symlink(link: &Link) -> Result<(), std::io::Error> {
+fn create_symlink(link: &Link) -> anyhow::Result<()> {
     // Make sure parent path exists
-    println!(
+    log::info!(
         "Creating parent directory (if needed) {:?}",
         link.parent_path
     );
     fs::create_dir_all(&link.parent_path)?;
 
-    println!(
+    log::info!(
         "Removing existing install location (if needed) {:?}",
         link.install_path
     );
@@ -209,41 +196,40 @@ fn symlink(link: &Link) -> Result<(), std::io::Error> {
         _ => {}
     } // ignore error
 
-    println!(
+    log::info!(
         "Creating link {:?} -> {:?}",
-        link.install_path, link.target_path
+        link.install_path,
+        link.target_path
     );
     fs::soft_link(&link.target_path, &link.install_path)?;
     Ok(())
 }
 
-fn install(links: &Links) -> Result<(), std::io::Error> {
+fn install(links: &[Link]) -> anyhow::Result<()> {
     for link in links {
-        symlink(link)?;
+        create_symlink(link)?;
     }
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> anyhow::Result<()> {
+    log::set_logger(&Logger)
+        .map(|()| log::set_max_level(log::LevelFilter::Info))
+        .map_err(|e| anyhow::anyhow!(e))?;
+
     let opt = Opt::from_args();
-    let mut dotfile_toml_path = opt.profile.clone();
-    dotfile_toml_path.push("dotfile.toml");
+    let dotfile_toml_path = opt.profile.join("dotfile.toml");
 
-    println!("Parsing config {:?}", dotfile_toml_path);
+    log::info!("Parsing config {:?}", dotfile_toml_path);
 
-    let dotfile_toml = fs::read_to_string(dotfile_toml_path)?;
+    let dotfile_toml = fs::read_to_string(&dotfile_toml_path)?;
     let profile: Profile = toml::from_str(&dotfile_toml)?;
 
     if opt.verbose {
-        dbg!(&profile);
+        log::info!("Parsed profile: {profile:#?}");
     }
 
-    let (warns, errs, links) = validate(&opt.profile, &profile);
-    println!("{warns} warnings found, {errs} errors found.");
-
-    if errs > 0 {
-        return Err(Box::new(ValidationError));
-    }
+    let links = validate(&opt.profile, &profile)?;
 
     if opt.install {
         install(&links)?;
