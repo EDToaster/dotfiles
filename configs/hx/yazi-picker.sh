569c1935276cc50c46b3ebf6a14830728f9a304a
EDToaster <howard3061@gmail.com>
2025-03-25 22:18:22 -0700

    Add helix inline warnings, add file picker using yazi

---
+++ configs/hx/yazi-picker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FIFO="/tmp/yazi-fifo-$$"
+mkfifo "$FIFO"
+
+zellij run -n Yazi -c -f -x 10% -y 10% --width 80% --height 80% -- \
+	bash -c "
+		# Open FIFO for reading and writing
+		exec 3<> '$FIFO'
+		if [ -n '$1' ]; then
+    	yazi --chooser-file '$FIFO' '$1'
+    else
+    	yazi --chooser-file '$FIFO'
+    fi
+		# Close the FIFO after yazi finishes
+		exec 3>&-
+  "
+
+if read -r line < "$FIFO"; then
+  echo "$line"
+else
+	exit 1
+fi
